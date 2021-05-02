360f312274b9d27f887696199f4f62176ed513aa
shivgaurav <shivgaurav27@gmail.com>
2021-05-02 15:37:24 +0530

    subheader added

--- src/components/SubHeader/index.js
+++ src/components/SubHeader/index.js
@@ -6,6 +6,10 @@ const metricStyles = makeStyles({
   root: {
     top: 20,
   },
+  rootStyle: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
   regularstyle: {
     minHeight: 48,
   },
@@ -16,8 +20,21 @@ function SubHeader(props) {
   return (
     <>
       <AppBar className={classes.root} position="relative" color="primary">
-        <Toolbar classes={{ regular: classes.regularstyle }}>
-          <Typography variant="h4">Date</Typography>
+        <Toolbar
+          classes={{ regular: classes.regularstyle, root: classes.rootStyle }}
+        >
+          <Typography variant="h5">
+            Date: 04/19/2020 at 5:20AM -04/19/2020 at 10:20AM (5 Hrs 0 Minutes)
+          </Typography>
+          <div className="d-flex justify-content-between">
+            <Typography variant="h5" style={{ color: "#0be0d9" }}>
+              Total KM :165 KM &nbsp;
+            </Typography>
+            <Typography variant="h5" style={{ color: "#c5e305" }}>
+              {" "}
+              Total Expenses : 0
+            </Typography>
+          </div>
         </Toolbar>
       </AppBar>
       <br />
