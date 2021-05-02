5120bef96f047045c0d1fd71d98b7401b79d6a35
shivgaurav <shivgaurav27@gmail.com>
2021-05-02 19:27:14 +0530

    trip summary

--- src/components/Trip/index.js
+++ src/components/Trip/index.js
@@ -1,15 +1,23 @@
-import { Typography } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
+import CustomDatePickr from "../common/CustomDatePickr";
+import "./Trip.css";
+const useStyles = makeStyles({
+  buttonStyle: {
+    borderRadius: "5px",
+    marginTop: "7px",
+  },
+});
 
 function Trip() {
+  const classes = useStyles();
   return (
     <div className="container-fluid px-4">
       <div className=" mt-5">
-        <div className="d-flex justify-content-between">
+        <div className="d-flex justify-content-between TripResponsiv">
           <div>
-            <Typography variant="h4" className="position-absolute">
-              Trip Summary
-            </Typography>
+            <Typography variant="h4">Trip Summary</Typography>
             <div className="d-flex justify-content-between mt-4">
               <Typography variant="body1">Dashboard</Typography>
               <Typography variant="body1" className="px-2">
@@ -18,7 +26,28 @@ function Trip() {
               <Typography variant="body1">Trip Summary</Typography>
             </div>
           </div>
-          <div>2</div>
+          <div className="d-flex justify-content-around ">
+            <div className="m-1">
+              <CustomDatePickr />
+            </div>
+
+            <div className="m-1">
+              <CustomDatePickr />
+            </div>
+            <div className="m-1 pt-4">
+              <Button
+                variant="outlined"
+                color="secondary"
+                size="small"
+                classes={{
+                  root: classes.buttonStyle,
+                  text: classes.buttonText,
+                }}
+              >
+                Export
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/common/CustomDatePickr/index.js
@@ -0,0 +1,39 @@
+import DateFnsUtils from "@date-io/date-fns";
+import Grid from "@material-ui/core/Grid";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import "date-fns";
+import React from "react";
+
+export default function CustomDatePickr() {
+  // The first commit of Material-UI
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date("2014-08-18T21:11:54")
+  );
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
+}
