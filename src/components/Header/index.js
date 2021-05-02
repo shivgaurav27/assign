2867ac06525e2257778de42efcef1d07b0a02213
shivgaurav <shivgaurav27@gmail.com>
2021-05-02 14:27:01 +0530

    header added

---
+++ src/components/Header/index.js
@@ -0,0 +1,135 @@
+import { Button } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "block",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  buttonStyle: {
+    border: "1px solid black",
+    backgroundColor: "#fff",
+  },
+  buttonLabel: {
+    color: "#000",
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <Button
+        color="inherit"
+        classes={{
+          root: classes.buttonStyle,
+          label: classes.buttonLabel,
+        }}
+      >
+        <PowerSettingsNewIcon />
+        Logout
+      </Button>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Material-UI
+          </Typography>
+
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <Button
+              color="inherit"
+              classes={{
+                root: classes.buttonStyle,
+                label: classes.buttonLabel,
+              }}
+            >
+              <PowerSettingsNewIcon />
+              Logout
+            </Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <Button
+              color="inherit"
+              classes={{
+                root: classes.buttonStyle,
+                label: classes.buttonLabel,
+              }}
+            >
+              <PowerSettingsNewIcon />
+              Logout
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+    </div>
+  );
+}
