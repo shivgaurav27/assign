01f9a21eca3211010a21f8e057d7d47dc327d4ae
shivgaurav <shivgaurav27@gmail.com>
2021-05-02 18:36:28 +0530

    duplicate data

--- src/components/TripData/index.js
+++ src/components/TripData/index.js
@@ -22,37 +22,14 @@ function TripData() {
       tripTime: "02hrs 00 min",
       odometerReading: "1020-10286",
     },
-    {
-      tripStartEnd: "IFFcO CHOWK(5:20AM) Dwarka(7:20AM)",
-      driverName: "ANDREW JOHN",
-      tripExpense: "0",
-      tripKm: "65",
-      tripGPSKm: "0.00",
-      tripTime: "02hrs 00 min",
-      odometerReading: "1020-10286",
-    },
-    {
-      tripStartEnd: "IFFcO CHOWK(5:20AM) Dwarka(7:20AM)",
-      driverName: "ANDREW JOHN",
-      tripExpense: "0",
-      tripKm: "65",
-      tripGPSKm: "0.00",
-      tripTime: "02hrs 00 min",
-      odometerReading: "1020-10286",
-    },
-    {
-      tripStartEnd: "IFFcO CHOWK(5:20AM) Dwarka(7:20AM)",
-      driverName: "ANDREW JOHN",
-      tripExpense: "0",
-      tripKm: "65",
-      tripGPSKm: "0.00",
-      tripTime: "02hrs 00 min",
-      odometerReading: "1020-10286",
-    },
   ];
   return (
     <div>
       <div className="p-4">
+        <SubHeader />
+        <StyleTable Data={Data} />
+        <SubHeader />
+        <StyleTable Data={Data} />
         <SubHeader />
         <StyleTable Data={Data} />
       </div>
